47fcccffa45a62fb77d0c73d93054ffc7fe670e2
Mfundo <mfundoknox@gmail.com>
2025-11-19 04:11:27 +0200

    data

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb4def507341b6274f6072f7202912a2c421813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa401214e5fa4a3886802421c4373ff15e6ee362")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
