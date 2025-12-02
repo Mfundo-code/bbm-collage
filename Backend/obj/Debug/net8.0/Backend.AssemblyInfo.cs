fe006252e8ce72b0934f68afcb67c24d33945369
Mfundo <mfundoknox@gmail.com>
2025-12-02 06:17:53 +0200

    dnem

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab85171ae22e0d9d6d0135875351bacc8b5a4594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b7d300acd4e20031af37ac7237fa97bbe33b32f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5112/api';
+const API_BASE_URL =  'http://192.168.0.137:5112/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
